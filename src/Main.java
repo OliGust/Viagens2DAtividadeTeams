ebd73189921e338cf99cb27360dbc279633f085f
Gustavo O. <ctt.gustavooliv@gmail.com>
2023-10-29 21:28:37 -0300

    Validação de CPF Atividade teams

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Turista mochileiro = new Turista();
         mochileiro.setNome("Adriano");
 
         Turista novoTurista = new Turista("Maria");
+
         novoTurista.setNome("Meire");
-        if(Util.ValidacaoCPF("123")){
-            novoTurista.setCpf("123");
-        } else{
-            System.out.println("Cpf inválido!");
+        System.out.println("Digite seu cpf (somente números): ");
+        Scanner tcd = new Scanner(System.in);
+        String cpf = tcd.nextLine();
+
+        if (Util.validaCPF(cpf)) {
+            System.out.println("CPF OK");
+        } else {
+            System.out.println("CPF Inválido!");
         }
 
+        tcd.close();
     }
 }
